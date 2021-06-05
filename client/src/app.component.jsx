ce5f30e2edf07592348d8a997bc665a29d31227e
thanhdat260997 <thanhdat201197@gmail.com>
2021-06-05 20:53:47 +0700

    update

---
+++ client/src/app.component.jsx
@@ -0,0 +1,23 @@
+import './App.css';
+import React, { Component } from 'react';
+import Header from './components/Header';
+import Product from './components/Product';
+class App extends Component {
+  render () {
+    return (
+     <div>
+        <Header />
+        <div className="row">
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+              <Product />
+            </div>
+            <div className="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+              <Product />
+            </div>
+          </div>
+       
+      </div>
+    );
+  }
+}
+export default App;
