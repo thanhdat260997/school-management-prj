331f6dc23a21a178e78ec467e1485064407ec27b
VuThuyThuy97 <vuthuy.uet@gmail.com>
2021-06-05 20:35:55 +0700

    update db account

--- server/db.js
+++ server/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-const MONGO_USERNAME = 'admin';
-const MONGO_PASSWORD = 'admin';
 const MONGO_HOSTNAME = '127.0.0.1';
 const MONGO_PORT = '27017';
 const MONGO_DB = 'mydb';
 
-const url = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const url = `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
 
 mongoose.connect(url, {useNewUrlParser: true});
